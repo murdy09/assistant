ea20150ea17a0427e694b5ac95e591932e462490
murdy09 <120402762+murdy09@users.noreply.github.com>
2026-02-05 19:53:47 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,18 @@
 import asyncio
+import threading
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "OK"
+
+def run_flask():
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
+
 
 async def main():
     import os
@@ -17,4 +31,5 @@ async def main():
     await bot.start_polling()
     
 if __name__ == "__main__":
+    threading.Thread(target=run_flask, daemon=True).start()
     asyncio.run(main())
